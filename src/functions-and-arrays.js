ced494e651f2edfddde7fa811f1b07e588046c6d
Miki Blasco <blascom@7@gmail.com>
2022-01-18 18:27:49 +0100

    commit testing lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,32 +1,57 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+ if(a > b) return a
+ else return b
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  let longWord = ""
+  if (words.length === 0) return null
+  else 
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longWord.length) {
+    longWord = words[i]}  
+  };
+  return longWord
+  
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+let suma = 0
+for (let i = 0; i < numbers.length; i++) {
+suma +=  numbers[i] }
+return suma
+
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
-
-
+function sum(numbers) {
+  if (numbers.length === 0) return 0
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+
+if (numbersAvg.length === 0)  return null
+  
+let average = sumNumbers(numbersAvg)/numbersAvg.length
+return average 
+}
 
 
 // Level 2: Array of strings
